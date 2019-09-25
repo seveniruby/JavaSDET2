b94e2a6693f8e73572d8d9d8296ef1e7a7132c95
seveniruby <seveniruby@gmail.com>
2019-09-25 21:06:34 +0800

    first testcase

--- src/test/java/TestJUnit4Demo.java
+++ src/test/java/TestJUnit4Demo.java
@@ -1,3 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class TestJUnit4Demo {
+    @Test
+    public void testDemo(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void testDemo2(){
+        assertTrue(false);
+    }
 
 }
