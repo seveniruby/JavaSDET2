9488cdca74aa7ec70425405517aef4b59f7a8a28
seveniruby <seveniruby@gmail.com>
2019-09-25 21:31:09 +0800

    add before beforeClass

--- src/test/java/TestJUnit4Demo.java
+++ src/test/java/TestJUnit4Demo.java
@@ -1,15 +1,47 @@
-import org.junit.Test;
+import org.junit.*;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.assertTrue;
 
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestJUnit4Demo {
+
+    @BeforeClass
+    public static void beforeAllTestCase(){
+        System.out.println("beforeAllTestCase");
+    }
+
+    @AfterClass
+    public static void afterAllTestCase(){
+        System.out.println("afterAllTestCase");
+    }
+
+    @Before
+    public void beforeTestCase(){
+        System.out.println("i am @before");
+    }
+
+    @After
+    public void afterTestCase(){
+        System.out.println("i am @after");
+    }
+
     @Test
-    public void testDemo(){
+    public void testDemo3(){
+        System.out.println("testDemo3");
+        assertTrue(false);
+    }
+
+    @Test
+    public void testDemo1(){
+        System.out.println("testDemo1");
         assertTrue(true);
     }
 
     @Test
     public void testDemo2(){
+        System.out.println("testDemo2");
         assertTrue(false);
     }
 
