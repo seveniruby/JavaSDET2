c94b3e371e221cd1d6ec97e2a748956578c279eb
seveniruby <seveniruby@gmail.com>
2019-10-22 22:05:04 +0800

    参数化例子

---
+++ src/test/java/TestDDRunner.java
@@ -0,0 +1,21 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class TestDDRunner {
+
+    @Test
+    @Parameters({
+            "17, false",
+            "18, true"
+    })
+    public void testDemo(int age, boolean valid){
+        assertThat(age>17, equalTo(valid));
+
+    }
+}
---
+++ src/test/java/TestParam.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(Parameterized.class)
+public class TestParam {
+
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][]{
+                {1, 2},
+                {3, 3},
+                {4, 5},
+                {6, 6},
+                {9, 10}
+        });
+    }
+
+    @Parameterized.Parameter(0)
+    public int actual;
+    @Parameterized.Parameter(1)
+    public int expection;
+
+    @Test
+    public void testDemo(){
+        assertThat("demo", actual, equalTo(expection));
+    }
+}
