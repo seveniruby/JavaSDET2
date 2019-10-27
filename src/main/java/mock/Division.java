3637e1adb4565b3ff61536c8667c3606fc9fb2c0
seveniruby <seveniruby@gmail.com>
2019-10-27 16:02:53 +0800

    覆盖率jacoco

---
+++ src/main/java/mock/Division.java
@@ -0,0 +1,19 @@
+package mock;
+
+public class Division {
+    public Integer divid(Integer a, Integer b) {
+        if (b == 0) {
+            return null;
+        } else if (a > 100) {
+            return null;
+        } else if (a < b) {
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
+
+    public Integer divid2(Integer a, Integer b, Integer c) {
+        return divid(a, b) / c;
+    }
+}
