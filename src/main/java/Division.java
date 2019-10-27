9ad7009ae5e617ff54fae7b806b02ecf64cfd386
seveniruby <seveniruby@gmail.com>
2019-10-27 14:37:56 +0800

    TDD与IDEA自带覆盖率

---
+++ src/main/java/Division.java
@@ -0,0 +1,14 @@
+public class Division {
+    public Integer divid(Integer a, Integer b){
+        if (b==0){
+            return null;
+        }else if(a>100){
+            return null;
+        }
+        else if (a<b){
+            return 0;
+        }else{
+            return a/b;
+        }
+    }
+}
---
+++ src/test/java/DivisionTest.java
@@ -0,0 +1,38 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+
+public class DivisionTest {
+
+    Division division=new Division();
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void divid() {
+        assertThat(division.divid(10, 5), equalTo(2));
+    }
+
+    @Test
+    public void dividReturnWithZero(){
+        assertThat(division.divid(1, 2), equalTo(0));
+    }
+
+    @Test
+    public void dividByZero(){
+        assertThat(division.divid(100, 0), equalTo(null));
+    }
+
+//    @Test
+//    public void dividWithAG100(){
+//        assertThat(division.divid(1000, 20), equalTo(null));
+//    }
+}
