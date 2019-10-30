51c7e11840e14c896f14666321df702a15619bca
seveniruby <seveniruby@gmail.com>
2019-10-30 21:52:36 +0800

    second po

--- src/test/java/selenium/page/ContactPage.java
+++ src/test/java/selenium/page/ContactPage.java
@@ -1,7 +1,13 @@
 package selenium.page;
 
+import org.openqa.selenium.By;
+
 public class ContactPage {
-    public void add(){
+    public ContactPage add(String username, String id, String phone){
+        App.driver.findElement(By.name("username")).sendKeys(username);
+        App.driver.findElement(By.name("acctid")).sendKeys(id);
+        App.driver.findElement(By.name("mobile")).sendKeys(phone);
+        return this;
 
     }
 
--- src/test/java/selenium/testcase/TestWeWork.java
+++ src/test/java/selenium/testcase/TestWeWork.java
@@ -8,11 +8,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import selenium.page.App;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestWeWork {
     public static String url="https://work.weixin.qq.com/";
     @Test
     public void testStart(){
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(url);
         driver.manage().window().maximize();
         driver.findElement(By.linkText("企业登录")).click();
@@ -23,10 +26,12 @@ public class TestWeWork {
         driver.manage().addCookie(new Cookie("wwrtx.sid", "PvmFAAW3_ZQOnOfp5SzMi6G2ksLVrBXqM1nz3SnTzUm58TfxXhPA3kekfJjTJMK1"));
 
         driver.navigate().refresh();
+
         App.driver=driver;
 
         App app=new App();
-        app.toMemberAdd().add();
+        String phone="15600534762";
+        app.toMemberAdd().add(phone, phone, phone);
 //        assertThat();
 
     }
