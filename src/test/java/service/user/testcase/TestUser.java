9cb969a76a1586d714750e748dd7c978e10e9613
seveniruby <seveniruby@gmail.com>
2020-01-04 17:25:06 +0800

    测试数据的数据驱动方法

--- src/test/java/service/user/testcase/TestUser.java
+++ src/test/java/service/user/testcase/TestUser.java
@@ -1,17 +1,31 @@
 package service.user.testcase;
 
+import app.page.BasePage;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
+import framework.PageObjectModel;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import service.user.api.User;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class TestUser {
 
@@ -85,6 +99,80 @@ public class TestUser {
 
         user.delete(userid).then().body("errcode", equalTo(0));
         user.get(userid).then().body("errcode", not(equalTo(0)));
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "TestUser.csv")
+    public void deleteByParams(String name, String userid) {
+        String nameNew = name;
+        if (userid.isEmpty()) {
+            userid = "seveniruby_" + System.currentTimeMillis();
+        }
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("name", nameNew);
+        data.put("department", new int[]{1});
+        data.put("mobile", String.valueOf(System.currentTimeMillis()).substring(0, 11));
+
+        User user = new User();
+        user.create(userid, data).then().body("errcode", equalTo(0));
+
+        user.delete(userid).then().body("errcode", equalTo(0));
+        user.get(userid).then().body("errcode", not(equalTo(0)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("deleteByParamsFromYamlData")
+    public void deleteByParamsFromYaml(String name, String userid, List<Integer> departs) {
+        String nameNew = name;
+        if (userid.isEmpty()) {
+            userid = "seveniruby_" + System.currentTimeMillis();
+        }
+        if(departs==null){
+            departs=Arrays.asList(1);
+        }
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("name", nameNew);
+        data.put("department", departs);
+        data.put("mobile", String.valueOf(System.currentTimeMillis()).substring(0, 11));
+
+        User user = new User();
+        user.create(userid, data).then().body("errcode", equalTo(0));
+
+        user.delete(userid).then().body("errcode", equalTo(0));
+        user.get(userid).then().body("errcode", not(equalTo(0)));
+    }
+
+    static Stream<Arguments> deleteByParamsFromYamlData() {
+
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        TypeReference<List<HashMap<String, Object>>> typeRef =
+                new TypeReference<List<HashMap<String, Object>>>() {
+                };
+//        String fileName = Thread.currentThread().getStackTrace()[1].getMethodName();
+
+        List<HashMap<String, Object>> data;
+        try {
+            data = mapper.readValue(
+                    TestUser.class.getResourceAsStream("TestUser.yaml"),
+                    typeRef);
+            ArrayList<Arguments> results = new ArrayList<>();
+            data.forEach(map -> {
+                results.add(arguments(
+                        map.get("name").toString(),
+                        map.get("userid").toString(),
+                        map.get("departs")
+                ));
+            });
+
+            return results.stream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Stream.of();
+
     }
 
     //todo: 获取部门成员列表
